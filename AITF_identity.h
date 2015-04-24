af1b7671d6ad92cbc4fb9ee8432d0c1e7ad09992
jjbreen <jjbreen@wpi.edu>
2015-04-24 15:00:46 -0400

    fixed up network identity

--- AITF_identity.h
+++ AITF_identity.h
@@ -5,18 +5,20 @@
 
 using namespace Tins;
 
-class AITF_packet{
+class AITF_identity{
 public:
-	AITF_identity(vector<RRFilter> rfilters, uint32_t dest_addr);
-	AITF_packet(uint32_t dest_addr);
-	AITF_packet(const uint8_t *data, uint32_t size);
+	AITF_identity(vector<RRFilter> rfilters, IP::address_type victim);
+	AITF_identity(const uint8_t *data, uint32_t size);
 
 	void addRRFilter(RRFilter rfil);
 	void serialize(uint8_t *data, uint32_t size) const;
+
+	vector<RRFilter> filters();
+	IP::address_type victim();
 	
 
 private:
-	uint32_t dest_addr;
+	IP::address_type victim;
 	vector<RRFilter> filters;
 
 }
